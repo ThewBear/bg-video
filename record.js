60571a9148002687ab65d0662fd1f2fe82e54bfd
ThewBear <26767607+ThewBear@users.noreply.github.com>
2021-03-18 20:04:49 +0700

    add workflows

--- record.js
+++ record.js
@@ -1,18 +1,39 @@
 const path = require("path");
+const fs = require("fs");
 const { chromium } = require("playwright");
+
+const urls = {
+  bg: `file:${path.join(__dirname, "scene.html")}`,
+  trends: "https://trends.google.com/trends/hottrends/visualize?nrow=3&ncol=3",
+};
 
 (async () => {
   const browser = await chromium.launch();
-  const page = await browser.newPage({
-    recordVideo: {
-      dir: ".",
-      size: { width: 1920, height: 1080 },
-    },
+  const videoPathsPromise = Object.keys(urls).map(async (key) => {
+    const url = urls[key];
+    const page = await browser.newPage({
+      recordVideo: {
+        dir: "videos",
+        size: { width: 1920, height: 1080 },
+      },
 
-    viewport: { width: 1920, height: 1080 },
+      viewport: { width: 1920, height: 1080 },
+    });
+
+    await page.goto(url);
+
+    await page.waitForTimeout(60 * 1000);
+    await page.close();
+    return [await page.video().path(), key];
   });
-  await page.goto(`file:${path.join(__dirname, "scene.html")}`);
-  //   await page.screenshot({ path: `example.png` });
-  await page.waitForTimeout(70 * 1000);
+
+  const videoPaths = await Promise.all(videoPathsPromise);
+
   await browser.close();
+
+  videoPaths.forEach((videoPath) => {
+    const ext = path.extname(videoPath[0]);
+    const dir = path.dirname(videoPath[0]);
+    fs.renameSync(videoPath[0], dir + "/" + videoPath[1] + ext);
+  });
 })();
